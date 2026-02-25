22406baa80a5e57904be8c5706b29603be1be711
David Toledo <toledo_1990@hotmail.com>
2026-02-25 22:55:22 +0100

    Initial commit: project structure and initial setup

---
+++ src/main/java/level1/ejercicio1/Main.java
@@ -0,0 +1,23 @@
+package level1.ejercicio1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args){
+
+        ArrayList<String> carBrands = new ArrayList<>(
+                List.of(
+                        "Subaru",
+                        "Lancia",
+                        "Ford",
+                        "Toyota",
+                        "Peugeot",
+                        "Mitsubishi",
+                        "Porsche",
+                        "Alpine")
+        );
+
+
+    }
+}
---
+++ src/main/java/level1/ejercicio1/StringFilter.java
@@ -0,0 +1,10 @@
+package level1.ejercicio1;
+
+import java.awt.*;
+
+public class StringFilter {
+
+    public List<String> filterStringsContainingO(List<String> strings);
+
+
+}
